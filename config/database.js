d89873f2b516fdebb453405dac3fae6f51bde4ec
saruhanoymaci <saruhan.oymaci@hotmail.com>
2025-03-23 21:10:59 +0300

    test deploy 2

--- config/database.js
+++ config/database.js
@@ -5,12 +5,7 @@ const connectDB = async () => {
     const MONGODB_URI =
       process.env.MONGODB_URI || "mongodb://localhost:27017/merndb";
 
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
-    const conn = await mongoose.connect(MONGODB_URI, options);
+    const conn = await mongoose.connect(MONGODB_URI);
     console.log("MongoDB bağlantısı başarılı");
   } catch (error) {
     console.error("MongoDB bağlantı hatası:", error.message);
